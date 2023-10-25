c126ef5bf6c70462c8915cc7d760b14d3b5d91f2
lethilinh61199 <lethilinh61199@gmail.com>
2023-10-25 22:37:44 +0700

    feat: check type select

--- lib/text_form_filed.dart
+++ lib/text_form_filed.dart
@@ -114,33 +114,6 @@ class CustomFormTextField extends StatefulWidget {
 class StateFormFiled extends State<CustomFormTextField> {
   @override
   Widget build(BuildContext context) {
-    /// [keyboardType = phone] validate [phone]
-    bool isCheckEmail(String value) {
-      bool isCheckEmail = true;
-      final RegExp regex = RegExp(
-          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)| (\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-
-      if (!regex.hasMatch(value)) {
-        // not valid email
-        isCheckEmail = false;
-      }
-
-      return isCheckEmail;
-    }
-
-    /// [keyboardType = phone] validate [email]
-    bool isCheckPhoneNumber(String value) {
-      bool isCheckPhone = true;
-      final RegExp regex = RegExp(r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)');
-
-      if (value.isEmpty || value.length > 11) {
-        isCheckPhone = false;
-      } else if (!regex.hasMatch(value)) {
-        isCheckPhone = false;
-      }
-
-      return isCheckPhone;
-    }
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
